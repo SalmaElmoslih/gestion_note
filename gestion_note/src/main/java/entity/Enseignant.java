a59ec9ac241286b444451057a0658464b6c8b15b
nizar <156302543+oceanicwind@users.noreply.github.com>
2024-01-12 08:47:29 +0100

    Update Enseignant.java

--- gestion_note/src/main/java/entity/Enseignant.java
+++ gestion_note/src/main/java/entity/Enseignant.java
@@ -1,5 +1,32 @@
-package entity;
+public class Enseignant extends personne {
+    private int idEnseignant;
+    private String nom;
+    private String prenom;
+    private List<Matiere> matieresEnseignees;
+    // Autres attributs...
 
-public class Enseignant {
+    public Enseignant(int idEnseignant, String nom, String prenom) {
+        this.idEnseignant = idEnseignant;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matieresEnseignees = new ArrayList<>();
+    }
 
+    public void ajouterNote(Etudiant etudiant, Matiere matiere, double valeur) {
+        Note nouvelleNote = new Note(etudiant, matiere, valeur);
+        etudiant.ajouterNote(nouvelleNote);
+    }
+
+public void consulterNotes(Matiere matiere, Etudiant etudiant) {
+        if (matiere != null && etudiant == null) {
+            
+            System.out.println("Notes pour la matière " + matiere.getNomMatiere() + ":");
+        } else if (matiere == null && etudiant != null) {
+
+            System.out.println("Notes pour l'étudiant " + etudiant.getNom() + " " + etudiant.getPrenom() + ":");
+        } else {
+            System.out.println("Veuillez spécifier soit une matière, soit un étudiant.");
+        }
+    }
 }
+
