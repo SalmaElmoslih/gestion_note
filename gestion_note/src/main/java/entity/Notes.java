b746e956345ed1ef4ebad076a3a0af34eec5de85
ymeriaf5 <87639959+ymeriaf5@users.noreply.github.com>
2024-01-12 08:54:57 +0100

    Update Notes.java

--- gestion_note/src/main/java/entity/Notes.java
+++ gestion_note/src/main/java/entity/Notes.java
@@ -1,5 +1,39 @@
 package entity;
+import javax.persistence.*;
 
+@Entity
 public class Notes {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String matiere;
+	private Double note;
+	public Notes() {
+	}
+	public Notes(Integer id, String matiere, Double note) {
+		this.id = id;
+		this.matiere = matiere;
+		this.note = note;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMatiere() {
+		return matiere;
+	}
+	public void setMatiere(String matiere) {
+		this.matiere = matiere;
+	}
+	public Double getNote() {
+		return note;
+	}
+	public void setNote(Double note) {
+		this.note = note;
+	}
+	
+	
 
 }
